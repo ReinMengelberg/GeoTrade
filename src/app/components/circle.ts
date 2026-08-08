277aa35d2472b7aca23817fb636d308f63853e8d
Teona Avdiieva <tavdiiev@f1r3s17.codam.nl>
2026-08-08 14:38:13 +0200

    Modify home button

---
+++ src/app/components/circle.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-circle',
+  styles: [ ``
+  ],
+  template: `
+    <div class="rounded-full bg-gray-500">
+  `,
+})
+export class Circle {
+
+}
