f3bd9c4268e4a466eaeb06662984905be1ffe54f
Teona Avdiieva <tavdiiev@f0r4s5.codam.nl>
2026-08-09 22:15:53 +0200

    Add login page

---
+++ src/routes/(auth)/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    let { children } = $props();
+</script>
+
+<svelte:head>
+    <title>GeoTrade - Login</title>
+</svelte:head>
+
+<div class="grid min-h-screen grid-cols-2">
+    <!-- Left side -->
+    <section class="flex items-start justify-start p-8">
+            <h1 class="text-4xl font-semibold">GeoTrade</h1>
+    </section>
+
+    <!-- Right side -->
+    <section class="flex items-start justify-center px-8 pt-40">
+        <div class="w-full max-w-sm">
+            {@render children()}
+        </div>
+    </section>
+</div>
---
+++ src/routes/(auth)/login/+page.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+    import * as Field from "$lib/components/ui/field/index.js";
+    import { Button } from "$lib/components/ui/button/index.js";
+    import { Input } from "$lib/components/ui/input/index.js";
+
+    let email = $state('');
+    let password = $state('');
+
+    function handleSubmit() {
+        console.log('Login:', email, password);
+    }
+</script>
+
+<h1 class="mb-6 text-2xl font-semibold">
+    Sign in to GeoTrade
+</h1>
+
+<form onsubmit={handleSubmit}>
+    <Field.Group>
+        <Field.Field>
+            <Field.Label for="email" class="text-base">
+                Email
+            </Field.Label>
+
+            <Input
+                id="email"
+                type="email"
+                bind:value={email}
+                required
+                class="h-8"
+            />
+        </Field.Field>
+
+        <Field.Field>
+            <Field.Label for="password" class="text-base">
+                Password
+            </Field.Label>
+
+            <Input
+                id="password"
+                type="password"
+                bind:value={password}
+                required
+                class="h-8"
+            />
+        </Field.Field>
+
+        <Button type="submit" class="h-10">
+            Sign in
+        </Button>
+    </Field.Group>
+</form>
