5f3ab49034ce8c321c0295a70382a0e1f74f7f48
Teona Avdiieva <tavdiiev@f1r4s16.codam.nl>
2026-08-13 17:37:10 +0200

    add temporary manual login authentication

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,20 @@
-import { building } from '$app/environment';
-import { auth } from '$lib/server/auth';
+// import { building } from '$app/environment';
+// import { auth } from '$lib/server/auth';
+// import type { Handle } from '@sveltejs/kit';
+// import { svelteKitHandler } from 'better-auth/svelte-kit';
+
+// export const handle: Handle = async ({ event, resolve }) => {
+// 	const session = await auth.api.getSession({ headers: event.request.headers });
+
+// 	event.locals.user = session?.user ?? null;
+// 	event.locals.session = session?.session ?? null;
+
+// 	return svelteKitHandler({ auth, event, resolve, building });
+// };
+
+// temp code for testing
 import type { Handle } from '@sveltejs/kit';
-import { svelteKitHandler } from 'better-auth/svelte-kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	const session = await auth.api.getSession({ headers: event.request.headers });
-
-	event.locals.user = session?.user ?? null;
-	event.locals.session = session?.session ?? null;
-
-	return svelteKitHandler({ auth, event, resolve, building });
+    return resolve(event);
 };
--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -2,13 +2,59 @@
     import * as Field from "$lib/components/ui/field/index.js";
     import { Button } from "$lib/components/ui/button/index.js";
     import { Input } from "$lib/components/ui/input/index.js";
-
+    import { signIn } from "$lib/auth-client";
     let email = $state('');
     let password = $state('');
 
-    function handleSubmit() {
-        console.log('Login:', email, password);
-    }
+    // function handleSubmit() {
+    //     console.log('Login:', email, password);
+    // }
+//         async function handleSubmit(event: SubmitEvent) {
+//         event.preventDefault();
+
+//         const response = await fetch('/api/auth/login', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify({
+//                 email,
+//                 password
+//             })
+//         });
+//         console.log('response status:', response.status);
+//         // const data = await response.json();
+
+//         // console.log(data);
+//         const text = await response.text();
+
+// console.log('response body:', text);
+//     }
+// async function handleSubmit(event: SubmitEvent) { 
+//     event.preventDefault(); 
+//     const result = await signIn.email({ email, password }); 
+//     console.log(result); 
+//     }
+
+async function handleSubmit(event: SubmitEvent) {
+    event.preventDefault();
+    // construct HTTP request and send it
+    const response = await fetch('/api/auth/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email,
+            password
+        })
+    });
+    
+    const data = await response.json();// take the response body and parse it as JSON
+
+    console.log('status:', response.status);
+    console.log('response:', data);
+}
 </script>
 
 <h1 class="mb-6 text-2xl font-semibold">
